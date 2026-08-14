894d9ea6f0ed7dc7115f08749877c3d5a3e619c7
Nikita Volkov <volkov.nikita@phystech.edu>
2026-08-14 11:10:35 +0500

    rm ref

--- ref/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
-
-python preprocessing_new_position.py test_17_07_26_kmk_15/kmk_15/
-python calc_darl.py
-python restore.py
